455a129f521daf0f5c5b5096399c14b85e94c06d
Stephen Hale <skhale@gmail.com>
2015-01-10 19:34:29 -0500

    Added db config for tests

---
+++ config/test/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=chelton-dashboard',
+    'charset' => 'utf8',
+    'username' => 'root',
+    'password' => ''
+];
